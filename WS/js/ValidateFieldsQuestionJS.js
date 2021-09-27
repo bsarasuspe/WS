3c27d9a4e3ea8de6f4987977f19bada5fe46b744
bsarasuspe <83415989+bsarasuspe@users.noreply.github.com>
2021-09-27 10:29:02 +0200

    lab02b

--- WS/js/ValidateFieldsQuestionJS.js
+++ WS/js/ValidateFieldsQuestionJS.js
@@ -1,14 +1,14 @@
 function validateForm() {
-  let eposta = document.forms["formularioa"]["eposta"].value;
-  let galdera = document.forms["formularioa"]["galdera"].value;
-  let zailtasuna = document.forms["formularioa"]["zailtasuna"].value;
-  var re = /([a-zA-Z]+[0-9]{3}@ikasle.ehu.(eus|es)|[a-zA-Z]*.*[a-zA-Z]+(@ehu.(eus|es)))/;
+  let eposta = document.forms["galderenF"]["eposta"].value;
+  let galdera = document.forms["galderenF"]["galdera"].value;
+  let zailtasuna = document.forms["galderenF"]["zailtasuna"].value;
+  var re = /^([a-zA-Z]+[0-9]{3}@ikasle.ehu.(eus|es)|[a-zA-Z]*.*[a-zA-Z]+(@ehu.(eus|es)))$/;
   if (re.test(eposta) == false){
     alert("E-posta ez da egokia!");
     return false;
   }
   if (galdera.length > 10){
-    alert("Galdera luzeegia da!");
+    alert("Galdera motzegia da!");
     return false;
   }
 }
--- WS/php/AddQuestion.php
+++ WS/php/AddQuestion.php
@@ -7,8 +7,19 @@
   <?php include '../php/Menus.php' ?>
   <section class="main" id="s1">
     <div>
-			Irudirik gabeko galdera bat gehitzeko PHP kodea
+      <?php $esteka = mysqli_connect ("localhost", "T52", "MjP0lBClwyn81", "db_T52") or die ("Errorea Dbra konektatzerakoan");
 
+      $sql="INSERT INTO questions(eposta, galdera, erZ, er01, er02, er03, zailtasuna, gaia, argazkia) 
+      VALUES ('$_POST[eposta]' ,'$_POST[galdera]', '$_POST[ezuzena]', '$_POST[eokerra1]', '$_POST[eokerra2]', '$_POST[eokerra3]', '$_POST[zailtasuna]', '$_POST[gaia]', null)";
+
+      if (!$esteka->query($sql)) {
+       die("Errore bat gertatu da. <p><a href='QuestionForm.php'> Saiatu beste galdera bat gehitzen.</a>");
+      }
+     
+      echo "Galdera bat gehitu da!";
+      echo "<p> <a href='ShowQuestions.php'> Galderak ikusi</a>";
+
+      mysqli_close($esteka); ?>
     </div>
   </section>
   <?php include '../html/Footer.html' ?>
