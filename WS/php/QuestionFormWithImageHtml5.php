6937254b3fbc7a6a0b282e2e8b81cc1bb8c04b02
bsarasuspe <83415989+bsarasuspe@users.noreply.github.com>
2021-09-29 16:21:01 +0200

    HTML5 zuzenketa

--- WS/php/QuestionFormWithImageHtml5.php
+++ WS/php/QuestionFormWithImageHtml5.php
@@ -7,34 +7,7 @@
 <body>
   <?php include '../php/Menus.php' ?>
   <section class="main" id="s1">
-    <div>
-      <div class="formularioa">
-            <h3>Formularioa</h3><br>
-            <form id="formularioa" name="formularioa" action="/action_page.php" onsubmit="return validateForm()">
-            <label for="eposta">(*) E-posta:</label><br>
-            <input type="text" id="eposta" name="eposta" value="" style="width:300px" required><br><br>
-            <label for="galdera">(*) Galdera:</label><br>
-            <input type="text" id="galdera" name="galdera" value="" style="width:300px" required><br><br>
-            <label for="ezuzena">(*) Erantzun zuzena:</label><br>
-            <input type="text" id="ezuzena" name="ezuzena" value="" style="width:300px" required><br><br>
-            <label for="eokerra1">(*) Erantzun okerra 1:</label><br>
-            <input type="text" id="eokerra1" name="eokerra1" value="" style="width:300px" required><br><br>
-            <label for="eokerra2">(*) Erantzun okerra 2:</label><br>
-            <input type="text" id="eokerra2" name="eokerra2" value="" style="width:300px" required><br><br>
-            <label for="eokerra3">(*) Erantzun okerra 3:</label><br>
-            <input type="text" id="eokerra3" name="eokerra3" value="" style="width:300px" required><br><br>
-            <label for="zailtasuna">(*) Zailtasuna:</label><br>
-            <select name="zailtasuna" style="width:300px">
-              <option value="1">Txikia</option>
-              <option value="2">Ertaina</option>
-              <option value="3">Handia</option>
-            </select><br><br>
-            <label for="gaia">(*) Gaia:</label><br>
-            <input type="text" id="gaia" name="gaia" value="" style="width:300px"><br><br>
-            <button type="submit" form="formularioa">Bidali</button>
-          </form>
-        </div>
-    </div>
+
   </section>
   <?php include '../html/Footer.html' ?>
 </body>
