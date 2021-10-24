ad5d9287320c82d4bcede22b3642933975b2275a
bsarasuspe <83415989+bsarasuspe@users.noreply.github.com>
2021-10-24 14:13:05 +0200

    hautazkoa 2

--- WS/js/GetUserInfoXml.js
+++ WS/js/GetUserInfoXml.js
@@ -1,4 +1,23 @@
-$("#formularioa").submit(function(datuak) {
+$(document).ready(function() {
+    $('#button').click(function() {
+        $('#telefonoa').val("a");
+        $('#izena').val("b");
+        $('#abizena').val("c");
+        $.get('../xml/Users.xml', function(datuak){
+            var epostenZer = $(datuak).find('eposta');
+            var eposta = $('#eposta').val();
+            for (var i = 0; i < epostenZer.length; i++){
+                if(epostenZer[i].childNodes[0].nodeValue == $eposta){
+                    $('#telefonoa').val("a");
+                    $('#izena').val("a");
+                    $('#abizena').val("a");
+                }
+            }
+        })
+    });
+});
+/*
+$(document).ready(function() {
     $.get('../xml/Users.xml', function(datuak){
         var epostenZer = $(datuak).find('eposta');
         var eposta = $('#eposta').val();
@@ -11,3 +30,5 @@ $("#formularioa").submit(function(datuak) {
         }
     })
 });
+
+*/
